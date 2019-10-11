75e9690fc5d116858d522f3f9dc522f7e032b79f
Gustavo <consugus@gmail.com>
2019-10-11 16:53:44 -0300

    Alpha-Vantaje-routes.js removed from declaration at routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,7 +6,6 @@ const app = express();
 //                     ROUTES
 // ================================================
 app.use ( require('./user-routes') );
-app.use ( require('./alpha-vantage-routes') );
 app.use ( require('./login-routes') );
 app.use ( require('../routes/cotization-routes') )
 
